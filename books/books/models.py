9a66c22d8b9fd061ff7512a57ecfd6e877ee5609
shakori999 <shakori999@gmail.com>
2022-02-17 16:34:50 +0300

    added a review model

--- books/books/models.py
+++ books/books/models.py
@@ -1,4 +1,6 @@
+from turtle import ondrag
 import uuid
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.db import models
 
@@ -18,3 +20,18 @@ class Book(models.Model):
 
     def get_absolute_url(self): # new
         return reverse('book_detail', args=[str(self.id)])
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review
