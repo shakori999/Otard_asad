93cd770107dc32c831b559d5e32870174a146091
shakori999 <shakori999@gmail.com>
2022-02-17 16:42:46 +0300

    add reviews to book's page

--- books/books/admin.py
+++ books/books/admin.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class ReviewInLine(admin.TabularInline):
+    model = Review
+
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewInLine,
+    ]
     list_display = ('title', 'author', 'price',)
 
 admin.site.register(Book, BookAdmin)
