c0b8b8652636cb60c85fe1e212adb9fb0e25bd83
shakori999 <shakori999@gmail.com>
2022-02-19 10:02:12 +0300

    basic search bar

--- books/books/views.py
+++ books/books/views.py
@@ -1,4 +1,4 @@
-from typing import List
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Book
@@ -26,3 +26,9 @@ class SeachResultsListView(ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
