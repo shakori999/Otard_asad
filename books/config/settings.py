c89a69f6c0db4386f998c940bf619da8610beb57
shakori999 <shakori999@gmail.com>
2022-02-19 19:01:22 +0300

    add debug toolbar

--- books/config/settings.py
+++ books/config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'allauth',
     'allauth.account',
+    'debug_toolbar',
 
     # Local
     'accounts',
@@ -78,6 +79,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -156,3 +158,8 @@ STATICFILES_FINDERS = [
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 DEFAULT_FROM_EMAIL = 'admin@Otard.com'
+
+# django-debug-toolbar
+import socket
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
