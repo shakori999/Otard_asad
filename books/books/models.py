75dd78205e296369bf026cdddd92e419a98fbfa6
shakori999 <shakori999@gmail.com>
2022-02-20 05:23:49 +0300

    add indexes to the book model

--- books/books/models.py
+++ books/books/models.py
@@ -15,6 +15,9 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
+        indexes = [
+            models.Index(fields=['id'], name='id_index'),
+        ]
         permissions = [ 
             ('special_status', 'Can read all books'),
         ]     
