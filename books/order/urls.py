a6b9888e57e0dd6ac17653a87c494ea0cda75748
shakori999 <shakori999@gmail.com>
2022-02-24 20:16:15 +0300

    incre/decre from summary

--- books/order/urls.py
+++ books/order/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('add-to-cart/<uuid:pk>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<uuid:pk>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-item-from-cart/<uuid:pk>/', remove_single_item_from_cart, name='remove-item-from-cart'),
     path('order-summary/', OrderSummaryView.as_view(), name='order_summary'),
 ]
--- books/order/views.py
+++ books/order/views.py
@@ -55,7 +55,7 @@ def add_to_cart(request, pk):
             )
         order.items.add(order_item)
         messages.info(request, "this item was added to your cart")
-    return redirect("book_detail", pk=pk)
+    return redirect("order_summary")
 
 def remove_from_cart(request, pk):
     item = get_object_or_404(Book, id=pk)
@@ -72,20 +72,42 @@ def remove_from_cart(request, pk):
                 user=request.user,
                 ordered=False
             )[0]
-            if order_item.quantity > 1:
-                order_item.quantity -= 1
-                order_item.save()
-                messages.info(request, "this item quantity was updated to your cart")
-                return redirect("book_detail", pk=pk)
-            else:
-                messages.info(request, "this item was removed to your cart")
-                order.items.remove(order_item)
-                order_item.delete()
-                return redirect("book_detail", pk=pk)
+            order.items.remove(order_item)
+            order_item.delete()
+            messages.info(request, "this item was removed to your cart")
+            return redirect("order_summary")
         else:
             messages.info(request, "this item was not in your cart")
             return redirect("book_detail", pk=pk)
-
     else:
         messages.info(request, "you don't have an active order")
+        return redirect("book_detail", pk=pk)
+    
+def remove_single_item_from_cart(request, pk):
+    item = get_object_or_404(Book, id=pk)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__id=item.id).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            messages.info(request, "This item quantity was updated.")
+            return redirect("order_summary")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("book_detail", pk=pk)
+    else:
+        messages.info(request, "You do not have an active order")
         return redirect("book_detail", pk=pk)
