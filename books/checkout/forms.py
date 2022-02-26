bb9f48e1c9702869494d76c1f5b57abd8624cec5
shakori999 <shakori999@gmail.com>
2022-02-26 05:35:38 +0300

    add a custom form

--- books/checkout/forms.py
+++ books/checkout/forms.py
@@ -1,5 +1,23 @@
+from urllib import request
 from django import forms
+from django_countries.fields import CountryField
 
+PAYMENT_CHOICES = (
+    ('S', 'Stripe'),
+    ('P', 'PayPal')
+)
 class CheckoutForm(forms.Form):
-    street_address = forms.CharField()
-    apartment_address = forms.CharField(required=False)
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '1234 Main st'
+    }))
+    apartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'placeholder': 'Apartment or suite'
+    }))
+    country = CountryField(blank_label='(select country)').formfield(attrs={
+        'class': 'custom-select d-block w-100'
+    })
+    ZIP = forms.CharField()
+    same_billing_address = forms.BooleanField(required=False)
+    save_info = forms.BooleanField(required=False)
+    payment_option = forms.ChoiceField(
+        widget=forms.RadioSelect(), choices=PAYMENT_CHOICES)
--- books/checkout/views.py
+++ books/checkout/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from urllib import request
+from django.shortcuts import redirect, render
+from django.views.generic import View
+from .forms import CheckoutForm
 # Create your views here.
 
-class CheckoutPageView(TemplateView):
-    template_name = 'checkout/checkout-page.html'
+class CheckoutPageView(View):
+    
+    def get(self, *args, **kwargs):
+        form = CheckoutForm()
+        context = {
+            'form': form
+        }
+        return render(self.request, 'checkout/checkout-page.html', context)
+
+    def post(self, *args, **kwargs):
+        form = CheckoutForm(request.POST or None)
+        if form.is_valid():
+            print("The form is valid")
+            return redirect('checkout')
