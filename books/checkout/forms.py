2998c18aad405da6c147ca59154e7345944aab90
shakori999 <shakori999@gmail.com>
2022-02-26 05:02:40 +0300

    installed django countries pacakge

---
+++ books/checkout/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class CheckoutForm(forms.Form):
+    street_address = forms.CharField()
+    apartment_address = forms.CharField(required=False)
--- books/config/settings.py
+++ books/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'debug_toolbar', 
+    'django_countries',
 
     # Local
     'accounts',
