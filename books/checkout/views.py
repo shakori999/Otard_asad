6e8cd8ac17b7f51baa8217d8e68a3ca5604cf3ef
shakori999 <shakori999@gmail.com>
2022-02-28 22:23:35 +0300

    made a simple strip example works

--- books/checkout/views.py
+++ books/checkout/views.py
@@ -1,3 +1,5 @@
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import redirect, render
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import View
@@ -12,6 +14,8 @@ import stripe
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+products = stripe.Product.list()
+prices = stripe.Price.list()
 class CheckoutPageView(View):
     
     def get(self, *args, **kwargs):
@@ -57,3 +61,63 @@ class CheckoutPageView(View):
 class PaymentView(View):
     def get(self, *args, **kwargs):
         return render(self.request, 'payments/payment.html')
+
+# def sync_products_view(request):
+#     print(products.data)
+#     print(prices.data)
+#     return HttpResponse('data')
+
+class StripeCheckoutView(View):
+    def post(self, request):
+        YOUR_DOMAIN = 'http://localhost:4242'
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                line_items=[
+                    {
+                        'price': '{{PRICE_ID}}',
+                        'quantity': 1,
+                    },
+                ],
+                payment_method_types=['card',],
+                mode='payment',
+                success_url=YOUR_DOMAIN + '/success=true&session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=YOUR_DOMAIN + '?canceled=true',
+            )
+            return redirect(checkout_session.url)
+        except Exception as e:
+            return str(e)
+    
+    @csrf_exempt
+    def create_checkout_session(request):
+        if request.method == 'GET':
+            domain_url = 'http://localhost:8000/'
+            stripe.api_key = settings.STRIPE_SECRET_KEY
+            try:
+                # Create new Checkout Session for the order
+                # Other optional params include:
+                # [billing_address_collection] - to display billing address details on the page
+                # [customer] - if you have an existing Stripe Customer ID
+                # [payment_intent_data] - capture the payment later
+                # [customer_email] - prefill the email input in the form
+                # For full details see https://stripe.com/docs/api/checkout/sessions/create
+
+                # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+                checkout_session = stripe.checkout.Session.create(
+                    success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+                    cancel_url=domain_url + 'cancelled/',
+                    payment_method_types=['card'],
+                    mode='payment',
+                    line_items=[
+                        {
+                            'name': 'T-shirt',
+                            'quantity': 1,
+                            'currency': 'usd',
+                            'amount': '2000',
+                        }
+                    ]
+                )
+                # return JsonResponse({'sessionId': checkout_session['id']})
+                return redirect(checkout_session.url)
+            except Exception as e:
+                # return str(e)
+                return JsonResponse({'error': str(e)})
