bd1fdfc84a9bb760095af4d51cd00c0af144e816
shakori999 <shakori999@gmail.com>
2022-03-15 22:12:45 +0300

    made up pytest and selenium

--- books/accounts/models.py
+++ books/accounts/models.py
@@ -1,4 +1,3 @@
-import imp
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
---
+++ books/dashboard/tests/test_selenium_dashboard.py
@@ -0,0 +1,8 @@
+from tkinter import W
+
+import pytest
+from selenium.webdriver.common.keys import keys
+
+@pytest.mark.selenium
+def test_dashboard_admin_login():
+    pass
