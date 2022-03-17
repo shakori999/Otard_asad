23e808f973cda05cb0b9be30f336d14667a60736
shakori999 <shakori999@gmail.com>
2022-03-17 18:07:54 +0300

    change browser to chrome

--- books/dashboard/tests/test_selenium_dashboard.py
+++ books/dashboard/tests/test_selenium_dashboard.py
@@ -2,15 +2,12 @@ import pytest
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-
-# pytestmark = pytest.mark.django_db
-
 @pytest.mark.selenium
 def test_dashboard_admin_login(
-    live_server, db_fixture_setup, chrome_browser_instance
+    live_server, db_fixture_setup, firefox_browser_instance
 ):
 
-    browser = chrome_browser_instance
+    browser = firefox_browser_instance
 
     browser.get(("%s%s" % (live_server.url, "/bingo/login")))
 
@@ -23,31 +20,3 @@ def test_dashboard_admin_login(
     submit.send_keys(Keys.RETURN)
 
     assert "Site administration" in browser.page_source
-
-
-# import pytest
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
-
-# from tests.fixtures import create_admin_user
-
-# # @pytest.mark.selenium
-# # def test_creat_new_admin_user(create_admin_user):
-# #     assert create_admin_user.__str__() == 'admin'
-
-# @pytest.mark.selenium
-# def test_dashboard_admin_login(live_server, db_fixture_setup,chrome_browser_instance):
-
-#     browser = chrome_browser_instance
-
-#     browser.get(("%s%s" % (live_server.url, "/admin/login/")))
-
-#     user_name = browser.find_element(By.NAME, "username")
-#     user_password = browser.find_element(By.NAME, "password")
-#     submit = browser.find_element(By.XPATH, "//input[@value='Log in']")
-
-#     user_name.send_keys("admin")
-#     user_password.send_keys("password")
-#     submit.send_keys(Keys.RETURN)
-
-#     assert "Site administration" in browser.page_source
--- books/tests/selenium.py
+++ books/tests/selenium.py
@@ -1,33 +1,12 @@
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.firefox.options import Options
-# from selenium.webdriver.firefox.service import Service
-# from webdriver_manager.firefox import GeckoDriverManager
-
-# @pytest.fixture(scope="module")
-# def firefox_browser_instance(request):
-#     """
-#         Provide a selenium webdriver instance
-#     """
-#     options = Options()
-#     options.headless = False
-#     browser = webdriver.Firefox(
-#         service=Service(GeckoDriverManager().install()),
-#         options=options,
-#     )
-#     yield browser
-#     browser.close()
-
-import pytest 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="module")
-def chrome_browser_instance(request):
+def firefox_browser_instance(request):
     """
-        provivde a selenium webdriver instance
+        Provide a selenium webdriver instance
     """
-
     options = Options()
     options.headless = False
     browser = webdriver.Chrome(options=options)
