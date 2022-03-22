34130be3063bcf80312a3c279b6ca60bdccb7230
shakori999 <shakori999@gmail.com>
2022-03-22 05:53:34 +0300

    made product/inventory tests

--- books/inventory/models.py
+++ books/inventory/models.py
@@ -147,54 +147,6 @@ class Brand(models.Model):
     )
 
 
-class ProductAttribute(models.Model):
-    """
-    Product attribute table
-    """
-
-    name = models.CharField(
-        max_length=255,
-        unique=True,
-        null=False,
-        blank=False,
-        verbose_name=_("product attribute name"),
-        help_text=_("format: required, unique, max-255"),
-    )
-    description = models.TextField(
-        unique=False,
-        null=False,
-        blank=False,
-        verbose_name=_("product attribute description"),
-        help_text=_("format: required"),
-    )
-
-    def __str__(self):
-        return self.name
-
-
-class ProductAttributeValue(models.Model):
-    """
-    Product attribute value table
-    """
-
-    product_attribute = models.ForeignKey(
-        ProductAttribute,
-        related_name="product_attribute",
-        on_delete=models.PROTECT,
-    )
-    attribute_value = models.CharField(
-        max_length=255,
-        unique=False,
-        null=False,
-        blank=False,
-        verbose_name=_("attribute value"),
-        help_text=_("format: required, max-255"),
-    )
-
-    def __str__(self):
-        return f"{self.product_attribute.name} : {self.attribute_value}"
-
-
 class ProductInventory(models.Model):
     """
     Product inventory table
@@ -223,11 +175,6 @@ class ProductInventory(models.Model):
         Product, related_name="product", on_delete=models.PROTECT
     )
     brand = models.ForeignKey(Brand, related_name="brand", on_delete=models.PROTECT)
-    attribute_values = models.ManyToManyField(
-        ProductAttributeValue,
-        related_name="product_attribute_values",
-        through="ProductAttributeValues",
-    )
     is_active = models.BooleanField(
         default=True,
         verbose_name=_("product visibility"),
