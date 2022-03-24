2a66596c3b87ff6f1dae890fdd19302e4385c079
shakori999 <shakori999@gmail.com>
2022-03-24 05:11:08 +0300

    added a filter to product detail view

--- books/dashboard/views.py
+++ books/dashboard/views.py
@@ -1,7 +1,6 @@
-from itertools import product
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView, TemplateView
-
+from django.db.models import Count
 from inventory.models import *
 
 # Create your views here.
@@ -26,26 +25,31 @@ def product_by_category(request, category):
         "slug",
         "category__name",
         "product__store_price",
-        # "product__attributevalues",
     )
 
     return render(request, "category/product_by_category.html", {"data": data})
 
 
-# class product_detail(DetailView):
-#     model = Product
-#     context_object_name = "book"
-#     template_name = "books/product-page.html"
-#     login_url = "account_login"
 def product_detail(request, slug):
 
-    data = ProductInventory.objects.filter(product__slug=slug).values(
-        "id",
-        "sku",
-        "product__name",
-        "store_price",
-        "product_inventory__units",
-        # "productinventory__attributevalues",
+    filter_arguments = []
+
+    if request.GET:
+        for value in request.GET.values():
+            filter_arguments.append(value)
+
+    data = (
+        ProductInventory.objects.filter(product__slug=slug)
+        .filter(attribute_values__attribute_value__in=filter_arguments)
+        .annotate(num_tags=Count("attribute_values"))
+        .filter(num_tags=len(filter_arguments))
+        .values(
+            "id",
+            "sku",
+            "product__name",
+            "store_price",
+            "product_inventory__units",
+        )
     )
 
     return render(request, "books/product_detail.html", {"data": data})
