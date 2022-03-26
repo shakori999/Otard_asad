610771260e0a72f5d0d1683b5553ae98f1132bec
shakori999 <shakori999@gmail.com>
2022-03-26 14:22:01 +0300

    installed elasticsearch in docker successfully

---
+++ books/inventory/documents.py
@@ -0,0 +1,18 @@
+# from django_elasticsearch_dsl import Document
+# from django_elasticsearch_dsl.registries import registry
+
+# from .models import ProductInventory
+
+
+# @registry.register_document
+# class ProductInventoryDocument(Document):
+#     class Index:
+#         name = "productinventory"
+
+#     class Django:
+#         model = ProductInventory
+
+#         fields = [
+#             "id",
+#             "sku",
+#         ]
